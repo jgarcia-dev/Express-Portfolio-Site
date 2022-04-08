521cc96f58cd69874798af884c72595903ca70e4
John Garcia <johngarciadev@gmail.com>
2022-04-08 14:44:02 -0700

    Updated index route and page with projects info.

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
+    res.locals.projects = projects;
     res.render('index');
-
 });
 
 app.listen(3000, ()=> {
