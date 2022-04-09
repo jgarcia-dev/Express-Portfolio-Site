c2405c8315a50ef39e378fe86550c460d2903593
John Garcia <johngarciadev@gmail.com>
2022-04-09 09:23:33 -0700

    Added project route and updated template.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { render } = require('express/lib/response');
 const data = require("./data.json");
 const { projects } = data;
 
@@ -10,6 +11,18 @@ app.use('/static', express.static('public'));
 app.get('/', (req, res) => {
     res.locals.projects = projects;
     res.render('index');
+});
+
+app.get('/project/:id', (req, res) =>  {
+    const projId = req.params.id;
+    const projectData = projects[projId];
+    
+    if (projectData) {
+        res.locals.projectData = projectData;
+        res.render('project');
+    } else {
+        res.send('404');
+    }
 });
 
 app.get('/about', (req, res) => {
