324d8ea8c0175afe250f728fac0fbd4c8714e45b
John Garcia <johngarciadev@gmail.com>
2022-04-10 09:21:09 -0700

    Added error handling for 404 and 500 errors.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { render } = require('express/lib/response');
 const data = require("./data.json");
 const { projects } = data;
 
@@ -13,7 +12,7 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/project/:id', (req, res) =>  {
+app.get('/project/:id', (req, res, next) =>  {
     const projId = req.params.id;
     const projectData = projects[projId];
     
@@ -21,12 +20,35 @@ app.get('/project/:id', (req, res) =>  {
         res.locals.projectData = projectData;
         res.render('project');
     } else {
-        res.send('404');
+        const err = new Error();
+        err.status = 404;
+        err.message = "Sorry, that project does not exist.";
+        next(err);
     }
 });
 
 app.get('/about', (req, res) => {
     res.render('about');
+});
+
+// 404 handler
+app.use((req, res, next) => {
+    const err = new Error();
+    err.status = 404;
+    err.message = "Sorry, that page does not exist.";
+    next(err);
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+        console.log('404 - Page does not exist');
+        res.status(404).render('page-not-found', { err });
+    } else {
+        err.status = err.status || 500;
+        err.message = err.message || 'Oops, looks like something went wrong on the server.';
+        res.status(err.status).render('error', { err });
+    }
 });
 
 app.listen(3000, ()=> {
